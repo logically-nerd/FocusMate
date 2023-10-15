a4fa664f8c051494b679450d7c4f3aadc6e0fbe0
Adnan Shahid <adnansh2804@gmail.com>
2023-10-15 17:00:31 +0530

    add services

--- services/background.js
+++ services/background.js
@@ -1,6 +1,23 @@
-console.log("background.js loaded")
+
+
+//0 if it is blocked 
+//1 if website is not blocked, 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'site') {
-        chrome.runtime.sendMessage({ type: "site", url: message.url });
-    }
+    const currURL = message.link
+    console.log(currURL)
+    chrome.storage.local.get('site', (data) => {
+        const linkArray = data.link
+        console.log(linkArray)
+        if (linkArray) {
+            for (let link of linkArray) {
+                if (currURL.includes(link)) {
+                    sendResponse({ message: 0 })
+                }
+                else {
+                    sendResponse({ message: 1 })
+                }
+            }
+        }
+    })
+    sendResponse({ message: 1 })
 })
