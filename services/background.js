eea28e323e6d62d2b4bca3c405007b9accb0b070
Tushar Saini <s2sharpit@gmail.com>
2023-10-15 16:57:39 +0530

    redesigning

--- services/background.js
+++ services/background.js
@@ -1,6 +1,2 @@
 console.log("background.js loaded")
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'site') {
-        chrome.runtime.sendMessage({ type: "site", url: message.url });
-    }
-})
+alert("Congratulations! You have finised a pomodoro session! :)");
