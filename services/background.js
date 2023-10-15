21f2f93504a537fa4fa88092e8cc4d00ff489a14
Adnan Shahid <adnansh2804@gmail.com>
2023-10-15 15:30:44 +0530

    background change greet update

--- services/background.js
+++ services/background.js
@@ -1,4 +1,6 @@
-setTimeout(toCheck, 30000)
-function toCheck() {
-    
-}
+console.log("background.js loaded")
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'site') {
+        chrome.runtime.sendMessage({ type: "site", url: message.url });
+    }
+})
